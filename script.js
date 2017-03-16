55aa2fd380bc1c43f201b6145f838c653e0f506b
Tristan Gilford <tristannica@tristannica.local>
2017-03-16 14:48:36 -0600

    restructured folder hierarchy for deployment

--- script.js
+++
@@ -1,3 +0,0 @@
-L.mapbox.accessToken = 'pk.eyJ1IjoidHJpc3Rhbm5pY2EiLCJhIjoieUhkQjdXRSJ9.Oed9lyoMW4UsOkWDnDyaog';
-    var map = L.mapbox.map('map', 'mapbox.satellite')
-    .setView([40, -98.0], 4);
